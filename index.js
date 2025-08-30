96bab4a7ade4aa6917f66dc68dadcbe224e3211f
aki_ooki <ookiakieml@gmail.com>
2025-08-31 05:16:00 +0900

    index.js参照の為、index.htmlを作成

---
+++ index.js
@@ -0,0 +1,17 @@
+let youtuber =  {
+    list: {
+        business: {
+            youtuber: [
+                {name:"しまぶ-", age: 20, teachProgramming() {} },
+                {name:"あっちゃん", age: 37, teachHistory() {} }
+            ],
+            teach() {}
+        },
+        entertainment: {
+            youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー" }],
+            makeSmile() {}
+        }
+    },
+    plan() {},
+    uploadVideo() {}
+};
