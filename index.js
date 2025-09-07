e0aa6fa36772daa17b1d50f70fd934c08df7f72b
aki_ooki <ookiakieml@gmail.com>
2025-09-08 04:40:21 +0900

    javascript  WebAPIを叩いてみよう！async await構文：2025/9/8

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-//const btn = document.getElementByID("button");
+async function callApi(){
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  console.log(users);
+}
 
-//btn.addEventListner("click", function() {
-//  console.log("フォロー解除");
-//});
-
-const foods = ["にんじん", "じゃがいも", "玉ねぎ"];
-
-foods.forEach(function (food) {
-  console.log(food);
-}); 
+callApi();
