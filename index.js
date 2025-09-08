f77d4f26f3bf97020c0f5bfd88db9dc0053973ca
aki_ooki <ookiakieml@gmail.com>
2025-09-09 04:19:52 +0900

    javascript  ボタンでデータ読込 async await構文：2025/9/9

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-async function callApi(){
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists");
+
+button.addEventListener("click", async function() {
+  // データのやり取り
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await res.json();
   console.log(users);
-}
 
-callApi();
+  // DOM操作
+});
