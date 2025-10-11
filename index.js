4c5d76fe05724d5279becd6db99eac0cbde31ae9
aki_ooki <ookiakieml@gmail.com>
2025-10-12 04:13:08 +0900

    javascript やまかわ_4：2025/10/12
    JavaScript入門 条件分岐を学ぼう！

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function greet(message) {
-    return message;
+let fruit = "バナナ";
+
+switch (fruit) {
+    case "バナナ":
+        console.log("バナナの値段は130円です!");
+        break;
+    case "メロン":
+        console.log("メロンの値段は200円です!");
+        break;
+    case "いちご":
+        console.log("いちごの値段は160円です!");
+        break;
+    default:
+        console.log("正しいフルーツを指定してください");
+        break;
 }
-
-const greetingmessage = greet("おはよう!");
-
-console.log(greetingmessage);
