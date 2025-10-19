b0437cd247e6a75a48bf4c749814a33bc366cedb
aki_ooki <ookiakieml@gmail.com>
2025-10-20 03:49:14 +0900

    javascript やまかわ_9：2025/10/20
    JavaScript入門 コールバック関数：関数の引数に渡す関数

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-// function を使った場合
-let getTriangle = function(base,height){
- return base * height / 2;
-};
-// アロー関数:=>(矢) を使って関数を記述
-let getTriangle2 = (base, height) => {
-  return base * height / 2;
-};
-// 本体が一文である場合、ブロックを表す{...}を省略できます。
-let getTriangle3 = (base, height) => base * height / 2;
+function sayhello(callback) {
+  callback();
+}
 
-console.log('getTriangle:三角形の面積は' + getTriangle(10,2));
-console.log('getTriangle2:三角形の面積は' + getTriangle2(10,2));
-console.log('getTriangle3:三角形の面積は' + getTriangle3(10,2));
+sayhello(function () {
+  console.log("Hello");
+});
 
-// 引数がひとつの場合、引数をくくるカッコも省略
-let getCircle = radius => radius * radius * Math.PI;
+console.log("1．データを取得中...");
 
-console.log(getCircle(10));
+setTimeout(function () {
+  console.log("2．データが取得されました!");
+}, 2000);
 
-// 引数がない場合はカッコを省略せずに記述
-let show = () => console.log('Hello world!');
-
-show(); 
+console.log("3．他の作業を続けます。");
