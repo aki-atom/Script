2df510f51d3cb0ce0f3cd7b5ac834da45f82b710
aki_ooki <ookiakieml@gmail.com>
2025-10-26 04:40:05 +0900

    javascript Code Tips_3：2025/10/26
    JavaScript ローカルストレージの使い方

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector("#container");
 const inputForm = document.querySelector("#inputForm");
 const saveBtn = document.querySelector("#saveBtn");
+const backBtn = document.querySelector("#backBtn");
+const deleteBtn = document.querySelector("#deleteBtn");
 
 // console.log(container);
 // console.log(inputForm);
@@ -11,4 +13,18 @@ saveBtn.addEventListener("click", ()=> {
   console.log(inputData);
   localStorage.setItem("key", inputData);
   inputForm.value = "";
+  // ブラウザを閉じてもデータは残る
+  // sessionStorageを使うと期限付きで残す事ができる
 });
+
+backBtn.addEventListener("click", () => {
+  const savedData = localStorage.getItem("key");
+  // console.log(savedData);
+  inputForm.value = savedData;
+});
+
+deleteBtn.addEventListener("click", ()=> {
+  localStorage.removeItem("key");
+  // 複数のkeyを削除する場合
+  // localStorage.clear();
+})
