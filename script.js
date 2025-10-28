838a9bc0edd5e086aaaf2623b4e7bd96816abdca
aki_ooki <ookiakieml@gmail.com>
2025-10-29 04:20:45 +0900

    javascript Code Tips_3_2：2025/10/29
    JavaScript キー操作の内容を検知する

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-const container = document.querySelector("#container");
-const inputForm = document.querySelector("#inputForm");
-const saveBtn = document.querySelector("#saveBtn");
-const backBtn = document.querySelector("#backBtn");
-const deleteBtn = document.querySelector("#deleteBtn");
-
-// console.log(container);
-// console.log(inputForm);
-// console.log(saveBtn);
-
-saveBtn.addEventListener("click", ()=> {
-  const inputData = inputForm.value;
-  console.log(inputData);
-  localStorage.setItem("key", inputData);
-  inputForm.value = "";
-  // ブラウザを閉じてもデータは残る
-  // sessionStorageを使うと期限付きで残す事ができる
+const text = document.querySelector(".text");
+const string = document.querySelector(".string");
+// keyupでキーボードから手が離れた回数を文字の入力と判断
+text.addEventListener("keyup", ()=> {
+  // 入力された値を取得
+  const inputText = text.value;
+  // 入力された値の長さを取得して表示
+  string.innerHTML = inputText.length;
 });
-
-backBtn.addEventListener("click", () => {
-  const savedData = localStorage.getItem("key");
-  // console.log(savedData);
-  inputForm.value = savedData;
+// keydownでキーボードを押したときのイベントを取得
+text.addEventListener("keydown", (e) => {
+  const key = e.keyCode;
+  console.log(key);
+  if (key === 39) {
+    console.log("右");
+  }
+  if (key === 37) {
+    console.log("左");
+  }
+  if (key === 13) {
+    console.log("Enter");
+  }
+  // shiftを押しながら Enterを押す
+  if (e.shiftKey && e.key === "Enter") {
+    console.log("shiftを押しながらEnter")
+  }
 });
-
-deleteBtn.addEventListener("click", ()=> {
-  localStorage.removeItem("key");
-  // 複数のkeyを削除する場合
-  // localStorage.clear();
-})
