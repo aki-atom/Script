56546d2aa9c7056671706f282888c60f4843ca20
aki_ooki <ookiakieml@gmail.com>
2025-10-30 04:30:08 +0900

    javascript Code Tips_4：2025/10/30
    JavaScript J画像を扱う方法

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-const text = document.querySelector(".text");
-const string = document.querySelector(".string");
-// keyupでキーボードから手が離れた回数を文字の入力と判断
-text.addEventListener("keyup", ()=> {
-  // 入力された値を取得
-  const inputText = text.value;
-  // 入力された値の長さを取得して表示
-  string.innerHTML = inputText.length;
-});
-// keydownでキーボードを押したときのイベントを取得
-text.addEventListener("keydown", (e) => {
-  const key = e.keyCode;
-  console.log(key);
-  if (key === 39) {
-    console.log("右");
-  }
-  if (key === 37) {
-    console.log("左");
-  }
-  if (key === 13) {
-    console.log("Enter");
-  }
-  // shiftを押しながら Enterを押す
-  if (e.shiftKey && e.key === "Enter") {
-    console.log("shiftを押しながらEnter")
-  }
-});
+const image = document.querySelector("#image");
+// すべてのimgタグのsrc属性にアクセスできる
+image.src = "./images/music1.jpg";
+// width、heightとするだけで、width属性とheight属性に
+// サイズを文字列でアクセス指定可能
+image.width = "800";
+image.height = "600";
