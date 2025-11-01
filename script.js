f4e82ed44ad0f3fdeacd944f90d510785a749cba
aki_ooki <ookiakieml@gmail.com>
2025-11-02 04:12:46 +0900

    javascript Code Tips_5：2025/11/02
    JavaScript 変数・定数(プログラミング入門講座)

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const image = document.querySelector("#image");
-// すべてのimgタグのsrc属性にアクセスできる
-image.src = "./images/music1.jpg";
-// width、heightとするだけで、width属性とheight属性に
-// サイズを文字列でアクセス指定可能
-image.width = "800";
-image.height = "600";
+// コンソールに表示する
+console.log("この帽子をお前に預ける");
+//変数let を利用
+let luffy; //letは変数宣言
+luffy = "人間";
+console.log(luffy);
+luffy = "ゴム人間";
+console.log(luffy);
+
+let bounty = 3000000;
+bounty = 100000000;
+console.log(bounty);
+
+//変数const 再代入できない
+const pirate_king = "ルフィ";
+console.log(pirate_king);
+
+//文字列に改行を含める
+console.log("一番イカつい\nクソババあ");
+
+//バッククォートを使うとそのまま改行できる
+let message;
+message = `いつかきっと返しに来い
+立派な海賊になってな`;
+console.log(message);
+
+//文字列に変数を埋め込む
+console.log(`おれは${luffy}だ!!`);
