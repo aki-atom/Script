5da790f8308ac2241aaa5d89bbbb67994967bb60
aki_ooki <ookiakieml@gmail.com>
2025-11-03 04:20:06 +0900

    javascript Code Tips_6：2025/11/03
    JavaScript 配列・変換(プログラミング入門講座)

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-// コンソールに表示する
-console.log("この帽子をお前に預ける");
-//変数let を利用
-let luffy; //letは変数宣言
-luffy = "人間";
-console.log(luffy);
-luffy = "ゴム人間";
-console.log(luffy);
+//配列
+let buggy_piraties = ["バギー","モージ","カバジ"];
 
-let bounty = 3000000;
-bounty = 100000000;
-console.log(bounty);
+console.log(buggy_piraties[0]);
+console.log(buggy_piraties[1]);
+console.log(buggy_piraties[2]);
+console.log(buggy_piraties[3]);
+console.log(buggy_piraties[-1]);
 
-//変数const 再代入できない
-const pirate_king = "ルフィ";
-console.log(pirate_king);
+//連想配列(ハッシュ) キーでアクセス
+let buggy = { age: 37,devil_fruit: "バラバラの実"};
+//参照：ブラケット記法
+console.log(buggy["age"]);
+console.log(buggy["devil_fruit"]);
+//参照：ドット記法
+console.log(buggy.age);
+console.log(buggy.devil_fruit);
 
-//文字列に改行を含める
-console.log("一番イカつい\nクソババあ");
+//文字列を数値に変換
+//Number()     数値以外を含む文字列はNaN(Not a Number)
+//parseInt()   数値に変換 数値以外を含む文字列はOK
+//parseFloat() 数値に変換 数値以外を含む文字列はOK
+let buggy_bounty = "15000000ベリー";
+console.log(Number(buggy_bounty));
+console.log(parseInt(buggy_bounty));
+console.log("型", typeof parseInt(buggy_bounty));
 
-//バッククォートを使うとそのまま改行できる
-let message;
-message = `いつかきっと返しに来い
-立派な海賊になってな`;
-console.log(message);
+console.log(parseInt("50.9ベリー"));
+console.log(parseFloat("50.9ベリー"));
+//値を文字列に変換 String()
+console.log(String(buggy["age"]));
+console.log("型", typeof String(buggy["age"]));
 
-//文字列に変数を埋め込む
-console.log(`おれは${luffy}だ!!`);
+//整数値であるかの判定 Number.isInteger()
+console.log(Number.isInteger(buggy["age"]));
+console.log(Number.isInteger(buggy_bounty));
+
+//配列であるかの判定 Array.isArray()
+console.log(Array.isArray(buggy_bounty));
+console.log(Array.isArray(buggy_piraties));
+console.log(Array.isArray(buggy));
