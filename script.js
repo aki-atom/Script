04de0d1c8b48945088da45a4c6d89c02cf15e5a4
aki_ooki <ookiakieml@gmail.com>
2025-11-05 04:42:20 +0900

    javascript エンジニアの休日：2025/11/05
    JavaScript 演算子(プログラミング入門講座)

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-//配列
-let buggy_piraties = ["バギー","モージ","カバジ"];
+//演算子
+let luffy_bounty = 100000000;
+let zoro_bounty = 60000000;
+let chopper_bounty = 50;
 
-console.log(buggy_piraties[0]);
-console.log(buggy_piraties[1]);
-console.log(buggy_piraties[2]);
-console.log(buggy_piraties[3]);
-console.log(buggy_piraties[-1]);
+console.log(luffy_bounty + zoro_bounty);
+console.log(luffy_bounty - zoro_bounty);
+console.log(luffy_bounty * 2);
+console.log((luffy_bounty + zoro_bounty) / 2);
+console.log(luffy_bounty % zoro_bounty);
+console.log(chopper_bounty ** 2);
+console.log(chopper_bounty + chopper_bounty);
+chopper_bounty = "50";
+//文字列の場合は連結する
+console.log(chopper_bounty + chopper_bounty);
+console.log("バギー" + "海賊団");
+//インクリメント演算子
+let chopper_age1 = 15;
+let chopper_age2 = chopper_age1++;  //代入してから加算
+console.log(chopper_age1);
+console.log(chopper_age2);
 
-//連想配列(ハッシュ) キーでアクセス
-let buggy = { age: 37,devil_fruit: "バラバラの実"};
-//参照：ブラケット記法
-console.log(buggy["age"]);
-console.log(buggy["devil_fruit"]);
-//参照：ドット記法
-console.log(buggy.age);
-console.log(buggy.devil_fruit);
-
-//文字列を数値に変換
-//Number()     数値以外を含む文字列はNaN(Not a Number)
-//parseInt()   数値に変換 数値以外を含む文字列はOK
-//parseFloat() 数値に変換 数値以外を含む文字列はOK
-let buggy_bounty = "15000000ベリー";
-console.log(Number(buggy_bounty));
-console.log(parseInt(buggy_bounty));
-console.log("型", typeof parseInt(buggy_bounty));
-
-console.log(parseInt("50.9ベリー"));
-console.log(parseFloat("50.9ベリー"));
-//値を文字列に変換 String()
-console.log(String(buggy["age"]));
-console.log("型", typeof String(buggy["age"]));
-
-//整数値であるかの判定 Number.isInteger()
-console.log(Number.isInteger(buggy["age"]));
-console.log(Number.isInteger(buggy_bounty));
-
-//配列であるかの判定 Array.isArray()
-console.log(Array.isArray(buggy_bounty));
-console.log(Array.isArray(buggy_piraties));
-console.log(Array.isArray(buggy));
+chopper_age1 = 15;
+chopper_age2 = ++chopper_age1;  //加算してから代入
+console.log(chopper_age1);
+console.log(chopper_age2);
+//演算した結果を代入する
+luffy_bounty = 100000000;
+luffy_bounty += 200000000;   //加算した結果を代入
+console.log(luffy_bounty);
