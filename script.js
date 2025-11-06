21b0184e9fcfb1ea15b637ce9c2c64f398346ef3
aki_ooki <ookiakieml@gmail.com>
2025-11-07 04:42:01 +0900

    javascript エンジニアの休日9：2025/11/07
    JavaScript 比較演算子(プログラミング入門講座)

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-//分割代入(配列の内容を別々の変数に代入)
-let devil_fruits = ["ゴムゴム","バラバラ","スベスベ"];
-let [Luffy, buggy, Alvida] = devil_fruits;
-console.log(Luffy);
-console.log(buggy);
-console.log(Alvida);
+//比較演算子
 
-[Luffy, buggy, Alvida, Usopp] = devil_fruits;
-console.log(Usopp);
+let zoro  = { age:21, height: 181 };
+let sanji = { age:21, height: 180 };
+// ==:値が等しいかどうか
+console.log("21 == 21 の結果");
+console.log( zoro.age == sanji.age );
+console.log("181 == 180 の結果");
+console.log( zoro.height == sanji.height );
+// ==:値が等しくないかどうか
+console.log("21 != 21 の結果");
+console.log( zoro.age != sanji.age );
+console.log("181 != 180 の結果");
+console.log( zoro.height != sanji.height );
+// < 左辺が右辺より小さいか
+console.log("181 < 180 の結果");
+console.log( zoro.height < sanji.height );
+console.log("180 < 181 の結果");
+console.log( sanji.height < zoro.height );
+console.log("21 < 21 の結果");
+console.log( zoro.age < sanji.age );
+// < 左辺が右辺以下
+console.log("21 <= 21 の結果");
+console.log( zoro.age <= sanji.age );
 
-devil_fruits = ["ゴムゴム","バラバラ","スベスベ","ヤミヤミ","グラグラ"];
-[Luffy, buggy, Alvida, Teach] = devil_fruits;
-console.log(Teach);
+// ===:値が等しくて、データの方も等しい
+console.log("21(数値) === 21(数値) の結果");
+console.log( zoro.age === sanji.age );
+console.log("21(文字列) === 21(数値) の結果");
+console.log( String(zoro.age) === sanji.age );
+console.log("21(文字列) == 21(数値) の結果_型は違ってもOK");
+console.log( String(zoro.age) == sanji.age );
+// == は内部変換してデータの型が違っても比較する
+// !== 値が等しくない または データの型が等しくない
+console.log("21(数値) !== 21(数値) の結果");
+console.log( zoro.age !== sanji.age );
+console.log("21(文字列) !== 21(数値) の結果_型は違ってもOK");
+console.log( String(zoro.age) !== sanji.age );
 
-// ...で残りのものを全て代入する
-[Luffy, buggy, Alvida, ...Teach] = devil_fruits;
-console.log(Teach);
-//変数のスワップ(2つの変数の中身を入れ替える)
-let sanji = "非能力者";
-let Absalom = "スケスケの実";
-console.log(sanji);
-console.log(Absalom);
-[sanji, Absalom] = [Absalom, sanji];
-console.log(sanji);
-console.log(Absalom);
-
-//オブジェクト(連想配列)でも分割代入は利用できる
-let Crocodile = {
-    age: 44, height: 253, devil_fruit: "スナスナの実"
-};
-//console.log(age); --> Errorになる
-//代入先は[]も(ブレケットではなく){}
-let { age, height, devil_fruit } = Crocodile;
-//キーと名前(中身)は一致させる
-console.log(age);
-console.log(height);
-console.log(devil_fruit);
+// ?: 条件演算子
+//条件によって値を切り替えたいときに利用
+// (条件式) ? 値1 : 値2
+//条件式がtrueなら値1、falseなら値2
+let volume = 30;
+let chopper_bounty;
+chopper_bounty = (volume <=44) ? 0:50
+console.log(`${volume}巻のときの懸賞金は`)
+console.log(chopper_bounty+"ベリー");
+volume = 47;
+chopper_bounty = (volume <=44) ? 0:50
+console.log(`${volume}巻のときの懸賞金は`)
+console.log(chopper_bounty+"ベリー");
