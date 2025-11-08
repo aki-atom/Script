0508d7ca61ef1e32d192ae3f2aa83ea2fc661f28
aki_ooki <ookiakieml@gmail.com>
2025-11-09 05:25:10 +0900

    javascript エンジニアの休日10：2025/11/09
    JavaScript  論理演算子(プログラミング入門講座)

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
-//比較演算子
+//論理演算子(&&, ||, !)
+// && 左側がtrueなら  右側を返す
+// && 左側がfalseなら 左側を返す
+let zoro =  { age: 21, height: 181};
+let sanji = { age: 21, height: 180};
+let luffy = { age: 19, height: 174};
+let usopp = { age: 19, height: 176};
 
-let zoro  = { age:21, height: 181 };
-let sanji = { age:21, height: 180 };
-// ==:値が等しいかどうか
-console.log("21 == 21 の結果");
-console.log( zoro.age == sanji.age );
-console.log("181 == 180 の結果");
-console.log( zoro.height == sanji.height );
-// ==:値が等しくないかどうか
-console.log("21 != 21 の結果");
-console.log( zoro.age != sanji.age );
-console.log("181 != 180 の結果");
-console.log( zoro.height != sanji.height );
-// < 左辺が右辺より小さいか
-console.log("181 < 180 の結果");
-console.log( zoro.height < sanji.height );
-console.log("180 < 181 の結果");
-console.log( sanji.height < zoro.height );
-console.log("21 < 21 の結果");
-console.log( zoro.age < sanji.age );
-// < 左辺が右辺以下
-console.log("21 <= 21 の結果");
-console.log( zoro.age <= sanji.age );
+console.log("21 == 21 && 19 == 19 の結果");
+console.log(zoro.age == sanji.age && luffy.age == usopp.age);
+console.log("21 == 21 && 19 < 19 の結果");
+console.log(zoro.age == sanji.age && luffy.age < usopp.age);
+console.log("21 < 21 && 19 == 19 の結果");
+console.log(zoro.age < sanji.age && luffy.age == usopp.age);
+console.log("21 == 21 && 174 の結果");
+console.log(zoro.age == sanji.age && luffy.height);
+// false:0, null, undefined, NaN, 空文字etc
+console.log("0 && 174 の結果");
+console.log(0 && luffy.height);
+// || 左側がtrueなら  左側を返す
+// || 左側がfalseなら 右側を返す
+console.log("21 == 21 || 19 == 19 の結果");
+console.log(zoro.age == sanji.age || luffy.age == usopp.age);
+console.log("21 < 21 || 19 == 19 の結果");
+console.log(zoro.age < sanji.age || luffy.age == usopp.age);
+console.log("21 < 21 || 19 < 19 の結果");
+console.log(zoro.age < sanji.age || luffy.age < usopp.age);
+console.log("21 < 21 || 174 の結果");
+console.log(zoro.age < sanji.age || luffy.height);
+console.log("0 || 174 の結果");
+console.log(0 || luffy.height);
+console.log("176 || 174 の結果");
+console.log(usopp.height || luffy.height);
 
-// ===:値が等しくて、データの方も等しい
-console.log("21(数値) === 21(数値) の結果");
-console.log( zoro.age === sanji.age );
-console.log("21(文字列) === 21(数値) の結果");
-console.log( String(zoro.age) === sanji.age );
-console.log("21(文字列) == 21(数値) の結果_型は違ってもOK");
-console.log( String(zoro.age) == sanji.age );
-// == は内部変換してデータの型が違っても比較する
-// !== 値が等しくない または データの型が等しくない
-console.log("21(数値) !== 21(数値) の結果");
-console.log( zoro.age !== sanji.age );
-console.log("21(文字列) !== 21(数値) の結果_型は違ってもOK");
-console.log( String(zoro.age) !== sanji.age );
+let pirate_king = "";
+pirate_king = pirate_king || "ルフィ";
+console.log(`海賊王になるのは ${pirate_king} だ!`);
 
-// ?: 条件演算子
-//条件によって値を切り替えたいときに利用
-// (条件式) ? 値1 : 値2
-//条件式がtrueなら値1、falseなら値2
-let volume = 30;
-let chopper_bounty;
-chopper_bounty = (volume <=44) ? 0:50
-console.log(`${volume}巻のときの懸賞金は`)
-console.log(chopper_bounty+"ベリー");
-volume = 47;
-chopper_bounty = (volume <=44) ? 0:50
-console.log(`${volume}巻のときの懸賞金は`)
-console.log(chopper_bounty+"ベリー");
+pirate_king = "白ひげ";
+pirate_king = pirate_king || "ルフィ";
+console.log(`海賊王になるのは ${pirate_king} だ!`);
+
+//!式がfalseの場合はtrue、trueの場合はfalse
+console.log("!(21 == 21) の結果");
+console.log(!(zoro.age == sanji.age));
+console.log("!(19 < 19) の結果");
+console.log(!(zoro.age < sanji.age));
+console.log("!(174) の結果");
+console.log(!(luffy.height));
+console.log("!(0) の結果");
+console.log(!(0));
