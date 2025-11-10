3207f7a82a5767138cf684d55a5e9845fb6516fc
aki_ooki <ookiakieml@gmail.com>
2025-11-11 03:48:19 +0900

    javascript エンジニアの休日11：2025/11/11
    JavaScript  条件分岐(プログラミング入門講座)

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-//論理演算子(&&, ||, !)
-// && 左側がtrueなら  右側を返す
-// && 左側がfalseなら 左側を返す
-let zoro =  { age: 21, height: 181};
-let sanji = { age: 21, height: 180};
-let luffy = { age: 19, height: 174};
-let usopp = { age: 19, height: 176};
+/* 条件分岐 if. else if, else
+if(条件1) {         条件1がtureなら処理1を実行
+    処理1            (それ以下は実行されない)
+} else if(条件2) {  条件2がtureなら処理2を実行
+    処理2            (それ以下は実行されない)
+} else {            どの行も満たさないとき処理3を実行
+    処理3
+} */
 
-console.log("21 == 21 && 19 == 19 の結果");
-console.log(zoro.age == sanji.age && luffy.age == usopp.age);
-console.log("21 == 21 && 19 < 19 の結果");
-console.log(zoro.age == sanji.age && luffy.age < usopp.age);
-console.log("21 < 21 && 19 == 19 の結果");
-console.log(zoro.age < sanji.age && luffy.age == usopp.age);
-console.log("21 == 21 && 174 の結果");
-console.log(zoro.age == sanji.age && luffy.height);
-// false:0, null, undefined, NaN, 空文字etc
-console.log("0 && 174 の結果");
-console.log(0 && luffy.height);
-// || 左側がtrueなら  左側を返す
-// || 左側がfalseなら 右側を返す
-console.log("21 == 21 || 19 == 19 の結果");
-console.log(zoro.age == sanji.age || luffy.age == usopp.age);
-console.log("21 < 21 || 19 == 19 の結果");
-console.log(zoro.age < sanji.age || luffy.age == usopp.age);
-console.log("21 < 21 || 19 < 19 の結果");
-console.log(zoro.age < sanji.age || luffy.age < usopp.age);
-console.log("21 < 21 || 174 の結果");
-console.log(zoro.age < sanji.age || luffy.height);
-console.log("0 || 174 の結果");
-console.log(0 || luffy.height);
-console.log("176 || 174 の結果");
-console.log(usopp.height || luffy.height);
+let episode;
+let chopper_bounty;
+// episode = 100;
+// episode = 500;
+// episode = 900;
+episode = 1100;
+if (episode <=435) {
+    chopper_bounty = 0;
+} else if (episode <= 800) {
+    chopper_bounty = 50;
+} else if (episode <= 1057) {
+    chopper_bounty = 100;
+} else {
+    chopper_bounty = 1000;
+}
+console.log(`${episode}話のチョッパーの懸賞金は`);
+console.log(`${chopper_bounty}ベリー`);
 
-let pirate_king = "";
-pirate_king = pirate_king || "ルフィ";
-console.log(`海賊王になるのは ${pirate_king} だ!`);
+/* switch 変数の値で処理を切り分ける
+switch (式) {
+  case 値1 :
+    処理1
+  case 値2 :
+    処理2
+  default :         合致するケースが見つからなかったとき
+    処理3
+}
+*/
 
-pirate_king = "白ひげ";
-pirate_king = pirate_king || "ルフィ";
-console.log(`海賊王になるのは ${pirate_king} だ!`);
+let luffy    = { name: "ルフィ", gender: "男"};
+let nami     = { name: "ナミ", gender: "女"};
+let vegapunk = { name: "ベガバンク", gender: "不明"};
 
-//!式がfalseの場合はtrue、trueの場合はfalse
-console.log("!(21 == 21) の結果");
-console.log(!(zoro.age == sanji.age));
-console.log("!(19 < 19) の結果");
-console.log(!(zoro.age < sanji.age));
-console.log("!(174) の結果");
-console.log(!(luffy.height));
-console.log("!(0) の結果");
-console.log(!(0));
+let person_gender = luffy.gender;
+console.log(luffy.name);
+switch (person_gender) {
+    case "男":
+        console.log("あなたは男です");
+        break;
+    case "女":
+        console.log("あなたは女です");
+        break;
+    default:
+        console.log("あなたは何者ですか?");
+        break;
+}
