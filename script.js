968dd54b7458f617d0e171cf187f4bfb27a9abb8
aki_ooki <ookiakieml@gmail.com>
2025-11-17 04:13:50 +0900

    javascript エンジニアの休日14：2025/11/17
    JavaScript  例外処理(プログラミング入門講座)

--- script.js
+++ script.js
@@ -1,66 +1,57 @@
-// for..in / for..of / for
-// for..in オブジェクトのプロパティ(キー)を順に取り出す
+// 例外処理 try..catch..finally
 /*
-  for ( prop(仮変数) in object(オブジェクト)) {
-    処理
-    オブジェクトからキーを取り出し、順番に仮変数に入る
-  }
-*/
-let Arlong = {    //アーロン
-    age: 39,
-    height: 263,
-    bounty: "200万",
-    laughter: "シャハハハ"
-};
-//propにキーが順番に入る、iでも良い
-for ( prop in Arlong) {
-  console.log(prop);
-}
-//キーと値を表示
-console.log("---for in(オブジェクト)---");
-for ( i in Arlong) {
-  console.log(`アーロンの${i}は「${Arlong[i]}」`);
-}
-
-//for..of 配列などの内容を取り出したい時に利用
-/*
-  for ( element(仮変数) of array(配列)) {
-    処理
-    配列から要素を取り出し、順番に変数に入る
+  try {
+    例外(エラー)が起きそうな処理  
+  } catch(exception) {
+    例外(エラー)が起きた時の処理
+  } finally {
+    例外(エラー)の有無に関係なく、実行
   }
 */
 
-//海軍大将
-let admiral_members = ["赤犬", "青雉", "黄猿"];
-console.log("--for of (配列)---");
-for ( value of admiral_members) {
-  console.log(value);
-}
+console.log("--例外が起きる---");
+  try {
+    console.log(one_peace);
+  } catch(exception) {
+    console.log("例外情報は⇩です");
+    console.log(exception.message);
+  } finally {
+    console.log("ワンピースの正体が気になります!");
+  }
+//例外が起きない例
+console.log("--例外が起きない---");
+  try {
+    console.log("one_peace");
+  } catch(ex) {
+    console.log("例外情報は⇩です");
+    console.log(ex.message);
+  } finally {
+    console.log("ワンピースの正体が気になります!");
+  }
+//例外を投げる(例外を発生させる) throw1
+console.log("--例外を投げる throw1---");
+let one_peace = "これまでに出会った仲間";
+  try {
+    console.log(one_peace);
+  } catch(ex) {
+    console.log("例外情報は⇩です");
+    console.log(ex.message);
+  } finally {
+    console.log("ワンピースの正体が気になります!");
+  }
 
-admiral_members = ["赤犬", "青雉", "黄猿"];
-console.log("--for ofを使わない---");
-for ( i = 0; i < 3; i++ ) {
-  console.log(admiral_members[i]);
-}
-admiral_members = ["赤犬", "青雉", "黄猿", "藤虎", "緑牛"];
-console.log("--for---");
-for ( i = 0; i < 5; i++ ) {
-  console.log(admiral_members[i]);
-}
-//ループから抜け出したいとき
-console.log("--for(breakあり)---");
-for ( i = 0; i < 5; i++ ) {
-  if (admiral_members[i] == "藤虎") {
-    break;   //ループ終了
+//例外を投げる(例外を発生させる) throw2
+console.log("--例外を投げる throw2---");
+one_peace = "これまでに出会った仲間";
+  try {
+    if (one_peace !== "") {
+      throw new TypeError("ワンピースの正体はまだ不明です");
+    }
+    console.log(one_peace);
+  } catch(ex) {
+    console.log("例外情報は⇩です");
+    console.log(ex.name);      //エラー名
+    console.log(ex.message);   //エラーメッセージ
+  } finally {
+    console.log("ワンピースの正体が気になります!");
   }
-  console.log(admiral_members[i]);
-}
-//特定のループをスキップしたいとき
-console.log("--for(continueあり)---");
-for ( i = 0; i < 5; i++ ) {
-  if (admiral_members[i] == "赤犬" ||
-      admiral_members[i] == "青雉") {
-    continue;   //現在のループのみスキップ
-  }
-  console.log(admiral_members[i]);
-}
