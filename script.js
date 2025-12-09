a9704ec9bf36ccfa6d6f461f92dc2f93e18676da
aki_ooki <ookiakieml@gmail.com>
2025-12-10 04:55:16 +0900

    javascript エンジニアの休日18：2025/12/10
    JavaScript  関数(プログラミング入門講座)

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
-//日付  Dateオブジェクト
+//関数   再利用したい処理をまとめておきたいとき
+/* funcution 関数名(引数名) {
+        処理(必要であれば引数を入れる)
+    }
+*/
 
-let date = new Date();
-console.log(date);
-//日付・時刻の要素を取得
-console.log("--getfullYear 年を取得");
-console.log(date.getFullYear());
-console.log("--getMonth 月を取得 (現在月-1)");
-console.log(date.getMonth());
 
-//形式を変換したい  toXxxxxxString()
-console.log("--toLocaleString--");
-console.log(date.toLocaleString());
+//ルフィの名言を言う関数
+//funcution命令  関数を直接定義
+function  luffy_saying() {
+    return "生きたいと言え!";
+}
+console.log("--function luffy_saying--");
+console.log(luffy_saying());
+//関数リテラル 名前のない関数を定義し、変数に代入
+let luffy_saying2 = function() {
+    return "ほらな折れねぇ!";
+};
+console.log("--関数リテラル luffy_saying2--");
+console.log(luffy_saying2());
 
-console.log("--toLocaleDateString--");
-console.log(date.toLocaleDateString());
+//違い
+console.log("--function命令 定義後ろ--");
+console.log(luffy_saying3());
+function  luffy_saying3() {
+    return "仲間がいる`よ!!!";
+}
+console.log("--匿名関数 定義後ろ--");
+//console.log(luffy_saying4());
+let luffy_saying4 = function() {
+    return "死ぬことは恩返しじゃねえぞ!!!";
+};
+console.log(luffy_saying4());
 
-console.log("--toLocaleTimeString--");
-console.log(date.toLocaleTimeString());
+//悪魔の実の名前を作成する関数
+function devil_fruit(word) {
+    return word.repeat(2) + "の実";
+}
+console.log("--devil_fruit関数--");
+console.log(devil_fruit("ゴム"));
+console.log(devil_fruit("オペ"));
 
-//加算・減算したい
-//getで取得し、setする
-console.log("--本日--");
-console.log(date.toLocaleDateString());
-console.log("--一ヶ月前--");
-date.setMonth(date.getMonth() - 1);
-console.log(date.toLocaleDateString());
-console.log("--3日後--");
-date.setDate(date.getDate() + 3);
-console.log(date.toLocaleDateString());
-console.log("--2年後--");
-date.setYear(date.getFullYear() + 2);
-console.log(date.toLocaleDateString());
+//関数リテラル  よりシンプルに!
+let luffy_saying5 = function () {
+    return "うるせぇ!!いこーーーう!";
+};
 
-//あと何日あるか知りたい
-let today = new Date(2022, 9, 9);
-//ワンピース104巻 発売日(11/4)
-let Release_date = new Date(2022, 10, 4);
-console.log("--104巻の発売日");
-console.log(Release_date.toLocaleDateString());
-
-//タイムスタンプを利用 getTime()
-//1970年1月1日からこれまでに経過した時間をミリ秒に変換した値
-console.log("--本日(2022/9/9)--");
-console.log(today.getTime());
-console.log("--発売日--");
-console.log(Release_date.getTime());
-let diff = Release_date.getTime() - today.getTime();
-console.log("--差は--");
-console.log(diff);
-console.log("--あと何日--");
-// 1000(秒) * 60(分) * 60(時間) * 24(日)
-let how_many_days = diff / (1000 * 60 * 60 * 24);
-console.log(how_many_days);
-console.log(`ワンピース発売日まであと${how_many_days}日`);
+//アロー関数
+let luffy_saying6 = () => {
+    return "うるせぇ!!いこーーーう!";
+};
+console.log(luffy_saying5());
+console.log("--アロー関数--");
+console.log(luffy_saying6());
+//一行なら{},rerurnも省略可能
+let luffy_saying7 = () => "うるせぇ!!いこーーーう!";
+console.log("--アロー関数省略ver--");
+console.log(luffy_saying7());
