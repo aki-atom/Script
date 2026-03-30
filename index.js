728e4fd377f8a1661dd50ceca5d430e8485927a2
aki_ooki <ookiakieml@gmail.com>
2026-03-31 04:32:24 +0900

    javascript エンジニアの休日29：2026/3/31
    JavaScript  要素の操作(プログラミング入門講座)

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-//フォームの検証(独自のエラー検証)
-let button = document.getElementById("button");
-let text = document.getElementById("text");
-let textError = document.getElementById("text_error");
+// 親要素：parentElement
+let luffy = document.getElementById("luffy");
+luffy.parentElement.style.backgroundColor = "pink";
 
-button.addEventListener("click", function (e) {
-  if (text.validity.valueMissing) {  //入力必須で値がないとtrue
-    textError.innerHTML = "入力しないと作成できないよ〜";
-    resultForm.textContent = "";
-  } else if (text.validity.tooShort) {  //下限を下回るときにtrue
-    textError.innerHTML = "文字数が少ないよ〜"
-    resultForm.textContent = "";
-  } else if (text.validity.patternMismatch){  //パターンに一致しないときに
-    textError.innerHTML = "すべて全角カタカナじゃないよ〜"
-    resultForm.textContent = "";
-  } else {
-  textError.innerHTML = ""
-  let textVlaue = text.value;
-  let result = textVlaue.repeat(2) + "の実"
-  let resultForm = document.getElementById("resultForm");
-  resultForm.textContent = `『${result}』`;
-  }
-  e.preventDefault();
-});
+// 子要素：children
+let parent = document.getElementById("parent");
+for(let i = 0; i < parent.children.length; i++){
+    parent.children[i].style.backgroundColor = "azure";
+}
+//子要素の一番最初：firstElementChild
+parent.firstElementChild.textContent = "firstElementChild => イチジ";
+//子要素の一番最後：lastElementChild
+parent.lastElementChild.textContent = "lastElementChild => ヨンジ";
+
+//前の要素：previousElementSibling
+//次の要素：nextElementSibling
+let zoro = document.getElementById("zoro");
+zoro.previousElementSibling.textContent = "previousElementSibling => ナミ"
+zoro.nextElementSibling.textContent = "nextElementSibling => ウソップ"
+
+//要素の作成：createElement
+let frineds = document.getElementById("frineds");
+let div = document.createElement("div");
+div.textContent = "appendChild => チョッパー";
+frineds.appendChild(div);
+
+//要素の置き換え：replaceChild(new_emperor, old)
+let emperors = document.getElementById("emperors");
+let old = document.getElementById("old");
+let new_emperor = document.createElement("div");
+new_emperor.textContent = "replaceChild => 黒ひげ";
+emperors.replaceChild(new_emperor, old);
+
+//要素の複製：cloneNode(子も含むか)
+let partner = document.getElementById("partner");
+let nami = document.getElementById("nami");
+let bon = nami.cloneNode(true);
+//falseだとナミというテキストが得られないから
+partner.appendChild(bon);
+
+//要素の削除：removeChild(子要素)
+partner.removeChild(nami);
