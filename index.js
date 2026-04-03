b52c234687e894d50a4726acca57410cebf1ff03
aki_ooki <ookiakieml@gmail.com>
2026-04-04 04:41:36 +0900

    javascript エンジニアの休日30：2026/4/4
    JavaScript  DocumentFragmentオブジェクト(プログラミング入門講座)

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-// 親要素：parentElement
-let luffy = document.getElementById("luffy");
-luffy.parentElement.style.backgroundColor = "pink";
+let chara_images = [
+    "img/nami.png",
+    "img/zoro.png",
+    "img/sanji.png",
+];
 
-// 子要素：children
-let parent = document.getElementById("parent");
-for(let i = 0; i < parent.children.length; i++){
-    parent.children[i].style.backgroundColor = "azure";
+let btn = document.getElementById("btn");
+let list = document.getElementById("list");
+
+//btn.addEventListener("click", NG_createlist);
+btn.addEventListener("click", OK_createlist);
+
+//何度も再描画(リストを追加)して負荷が高くなる
+function NG_createlist() {
+    chara_images.forEach(
+        function(path){
+            let li = document.createElement("li");
+            let img = document.createElement("img");
+            img.src = path;
+            li.appendChild(img);
+            list.appendChild(li);
+        }
+    );
 }
-//子要素の一番最初：firstElementChild
-parent.firstElementChild.textContent = "firstElementChild => イチジ";
-//子要素の一番最後：lastElementChild
-parent.lastElementChild.textContent = "lastElementChild => ヨンジ";
 
-//前の要素：previousElementSibling
-//次の要素：nextElementSibling
-let zoro = document.getElementById("zoro");
-zoro.previousElementSibling.textContent = "previousElementSibling => ナミ"
-zoro.nextElementSibling.textContent = "nextElementSibling => ウソップ"
+function OK_createlist() {
+    //フラグメントを生成する
+    let fragment = document.createDocumentFragment();
+    chara_images.forEach(
+        function (path) {
+            let li = document.createElement("li");
+            let img = document.createElement("img");
+            img.src = path;
+//            img.width = 300;
+//            img.height = 300;
+            li.appendChild(img);
+            //一時的にフラグメントに格納
+            fragment.appendChild(li);
+        }
+    );
+    //まとめてページに反映させる
+    list.appendChild(fragment);
+}
 
-//要素の作成：createElement
-let frineds = document.getElementById("frineds");
-let div = document.createElement("div");
-div.textContent = "appendChild => チョッパー";
-frineds.appendChild(div);
-
-//要素の置き換え：replaceChild(new_emperor, old)
-let emperors = document.getElementById("emperors");
-let old = document.getElementById("old");
-let new_emperor = document.createElement("div");
-new_emperor.textContent = "replaceChild => 黒ひげ";
-emperors.replaceChild(new_emperor, old);
-
-//要素の複製：cloneNode(子も含むか)
-let partner = document.getElementById("partner");
-let nami = document.getElementById("nami");
-let bon = nami.cloneNode(true);
-//falseだとナミというテキストが得られないから
-partner.appendChild(bon);
-
-//要素の削除：removeChild(子要素)
-partner.removeChild(nami);
