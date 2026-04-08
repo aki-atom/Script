74fe30f7d789e63a148820e52baf3aee984a5720
aki_ooki <ookiakieml@gmail.com>
2026-04-09 04:18:53 +0900

    javascript エンジニアの休日31 ：2026/4/9
    JavaScript  要素の入れ替え(プログラミング入門講座)

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
-let chara_images = [
-    "img/nami.png",
-    "img/zoro.png",
-    "img/sanji.png",
-];
-
 let btn = document.getElementById("btn");
 let list = document.getElementById("list");
 
-//btn.addEventListener("click", NG_createlist);
-btn.addEventListener("click", OK_createlist);
+btn.addEventListener("click", change_elements);
 
-//何度も再描画(リストを追加)して負荷が高くなる
-function NG_createlist() {
-    chara_images.forEach(
-        function(path){
-            let li = document.createElement("li");
-            let img = document.createElement("img");
-            img.src = path;
-            li.appendChild(img);
-            list.appendChild(li);
-        }
-    );
+function change_elements(){
+  //ルフィを複製
+  let luffy = list.firstElementChild.cloneNode(true);
+  //ナミを複製
+  let nami = list.lastElementChild.cloneNode(true);
+
+  //ナミとルフィ(複製)を置き換える
+  list.replaceChild(luffy, list.lastElementChild);
+  //ルフィとナミ(複製)を置き換える
+  list.replaceChild(nami, list.firstElementChild);
 }
-
-function OK_createlist() {
-    //フラグメントを生成する
-    let fragment = document.createDocumentFragment();
-    chara_images.forEach(
-        function (path) {
-            let li = document.createElement("li");
-            let img = document.createElement("img");
-            img.src = path;
-//            img.width = 300;
-//            img.height = 300;
-            li.appendChild(img);
-            //一時的にフラグメントに格納
-            fragment.appendChild(li);
-        }
-    );
-    //まとめてページに反映させる
-    list.appendChild(fragment);
-}
-
