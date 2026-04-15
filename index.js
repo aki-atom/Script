0420d4a34c77fcaf9bc825ec22b44f5fd867dc8c
aki_ooki <ookiakieml@gmail.com>
2026-04-16 04:42:17 +0900

    javascript エンジニアの休日32：2026/4/16
    JavaScript  スタイルの操作(プログラミング入門講座)

--- index.js
+++ index.js
@@ -1,16 +1,69 @@
 let btn = document.getElementById("btn");
+let reset = document.getElementById("reset");
 let list = document.getElementById("list");
+//ルフィを取得
+let luffy = list.firstElementChild;
 
-btn.addEventListener("click", change_elements);
+btn.addEventListener(
+  "click",
+  change_backgound_color
+);
 
-function change_elements(){
-  //ルフィを複製
-  let luffy = list.firstElementChild.cloneNode(true);
-  //ナミを複製
-  let nami = list.lastElementChild.cloneNode(true);
+reset.addEventListener(
+  "click",
+  reset_backgound_color
+);
 
-  //ナミとルフィ(複製)を置き換える
-  list.replaceChild(luffy, list.lastElementChild);
-  //ルフィとナミ(複製)を置き換える
-  list.replaceChild(nami, list.firstElementChild);
+function change_backgound_color(){
+  //styleプロパティを利用する
+  luffy.style.backgroundColor = "red";
 }
+
+function reset_backgound_color() {
+  //スタイルプロパティの初期化 null
+  luffy.style.backgroundColor = null;
+}
+
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let list2 = document.getElementById("list2");
+
+//ナミを取得
+let nami = list2.firstElementChild;
+
+btn2.addEventListener(
+  "click",
+  toggle_pink_class
+);
+
+function toggle_pink_class() {
+  //classListプロパティ
+  //toggleはクラスのオンオフを切り替え
+  nami.classList.toggle("pink");
+}
+
+//classNameプロパティ (上書き)
+// nami.className = "pink"
+
+btn3.addEventListener(
+  "click",
+  () => { //add クラスを追加
+    nami.classList.add("blue");
+  }
+);
+
+btn4.addEventListener(
+  "click",
+    () => { //add クラスを削除
+    nami.classList.remove("blue");
+  }
+);
+
+btn5.addEventListener(
+  "click",
+    () => { //replace(old, new) 置き換える
+    nami.classList.replace("pink", "blue");
+  }
+);
